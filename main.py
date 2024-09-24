e0e5a8a4e97cb76cfe2167d8ef1783837fee0b49
Keiran <keiran0@proton.me>
2024-09-24 02:52:20 +0100

    minor changes lol

--- main.py
+++ main.py
@@ -14,10 +14,7 @@ import json
 import os
 import shutil
 
-from modules.middleware import (
-    validate_file, handle_file_upload,
-    rate_limit
-)
+from modules.middleware import validate_file, handle_file_upload, rate_limit
 
 app = FastAPI()
 
@@ -38,7 +35,7 @@ file_locks = {}
 
 def load_json(file_path: str):
     try:
-        with open(file_path, 'r') as file:
+        with open(file_path, "r") as file:
             return json.load(file)
     except FileNotFoundError:
         return {}
@@ -48,17 +45,16 @@ def load_json(file_path: str):
 
 def save_json(file_path: str, data):
     try:
-        with open(file_path, 'w') as file:
+        with open(file_path, "w") as file:
             json.dump(data, file, indent=4)
     except IOError:
-        raise HTTPException(
-            status_code=500, detail="Error writing to JSON file")
+        raise HTTPException(status_code=500, detail="Error writing to JSON file")
 
 
 def init_globals():
     global file_delete_map, keys
     file_delete_map = load_json(DEL_FILE)
-    keys = {entry['key']: entry['user'] for entry in load_json(KEY_FILE)}
+    keys = {entry["key"]: entry["user"] for entry in load_json(KEY_FILE)}
 
 
 def acquire_lock(file_name: str) -> Lock:
@@ -68,7 +64,11 @@ def acquire_lock(file_name: str) -> Lock:
 
 
 def format_file_size(size_in_bytes: int) -> str:
-    return f"{size_in_bytes / 1024**2:.2f} MB" if size_in_bytes >= 1024**2 else f"{size_in_bytes / 1024:.2f} KB"
+    return (
+        f"{size_in_bytes / 1024**2:.2f} MB"
+        if size_in_bytes >= 1024**2
+        else f"{size_in_bytes / 1024:.2f} KB"
+    )
 
 
 def format_size(size_in_bytes: int) -> str:
@@ -82,7 +82,7 @@ def format_size(size_in_bytes: int) -> str:
 
 
 def generate_random_filename(length=8) -> str:
-    return ''.join(random.choices(string.ascii_uppercase + string.digits, k=length))
+    return "".join(random.choices(string.ascii_uppercase + string.digits, k=length))
 
 
 init_globals()
@@ -115,15 +115,18 @@ async def login(request: Request):
 async def get_server_info():
     upload_dir = Path(UPLOAD_DIR)
     total_storage_used = sum(
-        f.stat().st_size for f in upload_dir.glob('**/*') if f.is_file())
-    total_uploads = sum(1 for _ in upload_dir.glob('**/*') if _.is_file())
+        f.stat().st_size for f in upload_dir.glob("**/*") if f.is_file()
+    )
+    total_uploads = sum(1 for _ in upload_dir.glob("**/*") if _.is_file())
     total_users = sum(1 for _ in upload_dir.iterdir() if _.is_dir())
 
-    return JSONResponse(content={
-        "storage_used": format_size(total_storage_used),
-        "uploads": total_uploads,
-        "users": total_users
-    })
+    return JSONResponse(
+        content={
+            "storage_used": format_size(total_storage_used),
+            "uploads": total_uploads,
+            "users": total_users,
+        }
+    )
 
 
 @app.post("/upload")
@@ -132,33 +135,37 @@ async def upload(file: UploadFile = File(...), username: str = Depends(verify_ap
         rate_limit(username)
         validate_file(file)
         file_path, file_size, file_type, upload_time = handle_file_upload(
-            file, username, UPLOAD_DIR)
+            file, username, UPLOAD_DIR
+        )
         generated_name = generate_random_filename()
-        full_file_path = Path(UPLOAD_DIR) / username / \
-            f"{generated_name}{file_path.suffix}"
+        full_file_path = (
+            Path(UPLOAD_DIR) / username / f"{generated_name}{file_path.suffix}"
+        )
 
         try:
             shutil.move(str(file_path), str(full_file_path))
         except Exception as e:
-            raise HTTPException(
-                status_code=500, detail=f"Error moving file: {str(e)}")
+            raise HTTPException(status_code=500, detail=f"Error moving file: {str(e)}")
 
         if not full_file_path.exists():
             raise HTTPException(
-                status_code=500, detail="File could not be found after upload")
+                status_code=500, detail="File could not be found after upload"
+            )
 
         file_name_map[generated_name] = str(full_file_path)
         delete_uuid = str(uuid4())
         file_delete_map[delete_uuid] = str(full_file_path)
         save_json(DEL_FILE, file_delete_map)
 
-        return JSONResponse(content={
-            "file_url": f"{BASE_URL}/{generated_name}{full_file_path.suffix}",
-            "file-size": format_file_size(file_size),
-            "file-type": file_type,
-            "date-uploaded": upload_time,
-            "delete_url": f"{BASE_URL}/delete/{delete_uuid}"
-        })
+        return JSONResponse(
+            content={
+                "file_url": f"{BASE_URL}/{generated_name}{full_file_path.suffix}",
+                "file-size": format_file_size(file_size),
+                "file-type": file_type,
+                "date-uploaded": upload_time,
+                "delete_url": f"{BASE_URL}/delete/{delete_uuid}",
+            }
+        )
 
 
 @app.get("/uploads/{username}/{file_name}")
@@ -169,12 +176,15 @@ async def serve_embed(request: Request, username: str, file_name: str):
 
     file_url = f"{BASE_URL}/uploads/{username}/{file_name}"
 
-    return templates.TemplateResponse("embed.html", {
-        "request": request,
-        "file_name": file_name,
-        "file_url": file_url,
-        "username": username
-    })
+    return templates.TemplateResponse(
+        "embed.html",
+        {
+            "request": request,
+            "file_name": file_name,
+            "file_url": file_url,
+            "username": username,
+        },
+    )
 
 
 @app.get("/delete/{delete_uuid}")
@@ -188,8 +198,7 @@ async def delete_file(request: Request, delete_uuid: str):
     try:
         os.remove(file_path)
     except OSError as e:
-        raise HTTPException(
-            status_code=500, detail=f"Failed to delete file: {str(e)}")
+        raise HTTPException(status_code=500, detail=f"Failed to delete file: {str(e)}")
 
     return templates.TemplateResponse("file_deleted.html", {"request": request})
 
@@ -200,48 +209,68 @@ async def list_files(username: str = Depends(verify_api_key)):
     if not user_dir.exists():
         raise HTTPException(status_code=404, detail="User directory not found")
 
-    files = [{
-        "file_name": file.name,
-        "file_url": f"{BASE_URL}/uploads/{username}/{file.name}",
-        "file-size": format_file_size(file.stat().st_size),
-        "file-type": file.suffix[1:] if file.suffix else "unknown",
-        "date-uploaded": datetime.fromtimestamp(file.stat().st_ctime).strftime('%Y-%m-%d %H:%M:%S'),
-        "delete_url": f"{BASE_URL}/delete/{next((key for key, value in file_delete_map.items() if value == str(file.resolve())), 'N/A')}"
-    } for file in user_dir.glob("*") if file.is_file()]
+    files = [
+        {
+            "file_name": file.name,
+            "file_url": f"{BASE_URL}/uploads/{username}/{file.name}",
+            "file-size": format_file_size(file.stat().st_size),
+            "file-type": file.suffix[1:] if file.suffix else "unknown",
+            "date-uploaded": datetime.fromtimestamp(file.stat().st_ctime).strftime(
+                "%Y-%m-%d %H:%M:%S"
+            ),
+            "delete_url": f"{BASE_URL}/delete/{next((key for key, value in file_delete_map.items() if value == str(file.resolve())), 'N/A')}",
+        }
+        for file in user_dir.glob("*")
+        if file.is_file()
+    ]
 
     return JSONResponse(content={"files": files})
 
 
 @app.get("/search")
-async def search_files(query: str, username: str = Depends(verify_api_key)):
+async def search_files(
+    query: str, username: str = Depends(verify_api_key), limit: int = 10
+):
     user_dir = Path(UPLOAD_DIR) / username
     if not user_dir.exists():
         raise HTTPException(status_code=404, detail="User directory not found")
 
     file_names = [f.name for f in user_dir.glob("*") if f.is_file()]
-    matches = process.extract(query, file_names, limit=10)
+    matches = process.extract(query, file_names, limit=limit)
 
-    results = [{
-        "file_name": match[0],
-        "file_url": f"{BASE_URL}/uploads/{username}/{match[0]}",
-        "score": match[1]
-    } for match in matches]
+    results = [
+        {
+            "file_name": match[0],
+            "file_url": f"{BASE_URL}/uploads/{username}/{match[0]}",
+            "score": match[1],
+        }
+        for match in matches
+        if match[1] >= 60
+    ]
 
-    return JSONResponse(content={"results": results})
+    return JSONResponse(content={"results": results[:limit]})
 
 
 @app.post("/verify")
 async def verify_api_key_endpoint(authorization: str = Header(None)):
     try:
         username = await verify_api_key(authorization)
-        return JSONResponse(content={"status": "success", "message": "API key is valid", "username": username})
+        return JSONResponse(
+            content={
+                "status": "success",
+                "message": "API key is valid",
+                "username": username,
+            }
+        )
     except HTTPException as e:
-        return JSONResponse(status_code=e.status_code, content={"status": "error", "message": e.detail})
+        return JSONResponse(
+            status_code=e.status_code, content={"status": "error", "message": e.detail}
+        )
 
 
 @app.get("/{generated_filename}")
 async def serve_file(generated_filename: str):
-    original_file_path = file_name_map.get(generated_filename.split('.')[0])
+    original_file_path = file_name_map.get(generated_filename.split(".")[0])
 
     if not original_file_path or not os.path.exists(original_file_path):
         raise HTTPException(status_code=404, detail="File not found")
@@ -252,7 +281,10 @@ async def serve_file(generated_filename: str):
 
     return RedirectResponse(url=file_url)
 
+
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(app, host=os.getenv("HOST", "0.0.0.0"),
-                port=int(os.getenv("PORT", 8000)))
+
+    uvicorn.run(
+        app, host=os.getenv("HOST", "0.0.0.0"), port=int(os.getenv("PORT", 8000))
+    )
